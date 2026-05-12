f5de56a3a79326e0b3a4795e6df18de272cd85bf
Vikas Sharma <sharmavikas3525@gmail.com>
2026-05-12 19:01:35 +0530

    Added LCP238

---
+++ LCP238.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] a = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            a[i] = 1;
+        }
+
+        int left = 1;
+        for (int i = 0; i < nums.length; i++) {
+            a[i] *= left;
+            left *= nums[i];
+        }
+
+        int right = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            a[i] *= right;
+            right *= nums[i];
+        }
+
+        return a;
+    }
+}
