6e94edd25071482bd009a67a36053674dd674932
Vikas Sharma <sharmavikas3525@gmail.com>
2026-06-04 22:54:06 +0530

    Added LCP3898

---
+++ LCP3898.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int[] findDegrees(int[][] matrix) {
+        int n = matrix.length;
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (matrix[i][j] == 1)
+                    a[i]++;
+        return a;
+
+    }
+}
