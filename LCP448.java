85f7a5ed1e12da28583222616107885974a3d909
Vikas Sharma <sharmavikas3525@gmail.com>
2026-06-05 11:29:25 +0530

    Added LCP448

---
+++ LCP448.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        int i = 0;
+        while (i < nums.length) {
+            int correct = nums[i] - 1;
+            if (nums[i] != nums[correct]) {
+                swap(nums, i, correct);
+            } else {
+                i++;
+            }
+        }
+        List<Integer> a = new ArrayList<>();
+        for (int index = 0; index < nums.length; index++) {
+            if (nums[index] != index + 1)
+                a.add(index + 1);
+
+        }
+        return a;
+    }
+
+    private void swap(int[] nums, int first, int second) {
+        int temp = nums[first];
+        nums[first] = nums[second];
+        nums[second] = temp;
+    }
+}
