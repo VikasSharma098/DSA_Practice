c148ddfe82293570e73736e61178751cb794e7c6
Vikas Sharma <sharmavikas3525@gmail.com>
2026-06-16 20:16:36 +0530

    Added LCP3612

---
+++ LCP3612.java
@@ -0,0 +1,24 @@
+//LCP3612: Process String with Special Operations I
+//https://leetcode.com/problems/process-string-with-special-operations-i/?envType=daily-question&envId=2026-06-16
+class Solution {
+    public String processStr(String s) {
+        StringBuilder ans = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c >= 'a' && c <= 'z') {
+                ans.append(c);
+            } else if (c == '*') {
+                if (ans.length() > 0) {
+                    ans.deleteCharAt(ans.length() - 1);
+                }
+            } else if (c == '#') {
+                ans.append(ans.toString());
+            } else {
+                ans.reverse();
+            }
+        }
+
+        return ans.toString();
+
+    }
+}
