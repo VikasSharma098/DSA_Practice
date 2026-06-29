26e9d593f834cd2f6a4bfab4baa4d7bee5d05494
Vikas Sharma <sharmavikas3525@gmail.com>
2026-06-29 15:54:45 +0530

    Added LCP1967

---
+++ LCP1967.java
@@ -0,0 +1,12 @@
+//LCP1967: Number of Strings That Appear as Substrings in Word
+//https://leetcode.com/problems/number-of-strings-that-appear-as-substrings-in-word/?envType=daily-question&envId=2026-06-29
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+        int count = 0;
+        for (String s : patterns)
+            if (word.indexOf(s) != -1)
+                count++;
+        return count;
+
+    }
+}
