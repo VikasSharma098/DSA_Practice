470747c1fe56fccdcca64495d9719ed7bd45669f
Vikas Sharma <sharmavikas3525@gmail.com>
2026-07-10 11:39:21 +0530

    Added LCP492

---
+++ LCP492.java
@@ -0,0 +1,16 @@
+//LCP492: Construct the Rectangle
+//https://leetcode.com/problems/construct-the-rectangle/
+class Solution {
+    public int[] constructRectangle(int area) {
+        int[] ans = new int[2];
+        for (int i = (int) (Math.sqrt(area)); i >= 1; i--) {
+            if (area % i == 0) {
+                ans[1] = i;
+                ans[0] = area / i;
+                break;
+            }
+        }
+        return ans;
+
+    }
+}
