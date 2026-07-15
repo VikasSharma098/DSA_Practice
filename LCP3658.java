6411f6ea318957c7a91da964e2951db2fbc7e529
Vikas Sharma <sharmavikas3525@gmail.com>
2026-07-15 09:30:24 +0530

    Added LCP3658

---
+++ LCP3658.java
@@ -0,0 +1,6 @@
+//LCP3658: https://leetcode.com/problems/gcd-of-odd-and-even-sums/submissions/2068043319/?envType=daily-question&envId=2026-07-15
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return n;
+    }
+}
