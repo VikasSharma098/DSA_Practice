43f5f4446cd068fab9e6558e63e558019bd5a40a
Vikas Sharma <sharmavikas3525@gmail.com>
2026-07-31 16:07:28 +0530

    Added LCP390

---
+++ LCP390.java
@@ -0,0 +1,19 @@
+//LCP390: Elimination Game
+//https://leetcode.com/problems/elimination-game/
+class Solution {
+    public int lastRemaining(int n) {
+        boolean left = true;
+        int remaining = n;
+        int step = 1;
+        int head = 1;
+        while (remaining > 1) {
+            if (left || remaining % 2 == 1) {
+                head = head + step;
+            }
+            remaining = remaining / 2;
+            step = step * 2;
+            left = !left;
+        }
+        return head;
+    }
+}
