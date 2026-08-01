55935670305652e2afb69c9728c7a48e12cac35b
Vikas Sharma <sharmavikas3525@gmail.com>
2026-08-01 12:05:21 +0530

    Added LCP504

---
+++ LCP504.java
@@ -0,0 +1,11 @@
+//LCP504: Base 7
+//https://leetcode.com/problems/base-7/
+class Solution {
+    public String convertToBase7(int n) {
+        if (n < 0)
+            return "-" + convertToBase7(-n);
+        if (n < 7)
+            return Integer.toString(n);
+        return convertToBase7(n / 7) + Integer.toString(n % 7);
+    }
+}
