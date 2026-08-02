b37240bae3194e02acc92554aebf922302238ca0
Vikas Sharma <sharmavikas3525@gmail.com>
2026-08-02 10:54:08 +0530

    Added LCP877

---
+++ LCP877.java
@@ -0,0 +1,7 @@
+//LCP877: Stone Game
+//https://leetcode.com/problems/stone-game/?envType=daily-question&envId=2026-08-02
+class Solution {
+    public boolean stoneGame(int[] piles) {
+        return true;
+    }
+}
