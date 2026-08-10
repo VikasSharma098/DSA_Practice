5f567dfaee75cd3da56aab757b320d4a40d1324d
Vikas Sharma <sharmavikas3525@gmail.com>
2026-08-10 12:58:32 +0530

    Added LCP561

---
+++ LCP561.java
@@ -0,0 +1,12 @@
+//LCP561: Array Partition
+//https://leetcode.com/problems/array-partition/
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int result = 0;
+        for (int i = 0; i < nums.length; i += 2) {
+            result += nums[i];
+        }
+        return result;
+    }
+}
