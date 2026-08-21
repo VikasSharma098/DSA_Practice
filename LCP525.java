eaa49a1c5052dc9a1728b71e69859832f1200571
Vikas Sharma <sharmavikas3525@gmail.com>
2026-08-21 13:22:26 +0530

    Added LCP525

---
+++ LCP525.java
@@ -0,0 +1,32 @@
+//LCP 525. Contiguous Array
+//https://leetcode.com/problems/contiguous-array/
+
+public class Solution {
+
+    public int findMaxLength(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        map.put(0, -1);
+
+        int maxLen = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                count--;
+            }
+
+            if (map.containsKey(count)) {
+                maxLen = Math.max(maxLen, i - map.get(count));
+            } else {
+
+                map.put(count, i);
+            }
+        }
+
+        return maxLen;
+    }
+}
