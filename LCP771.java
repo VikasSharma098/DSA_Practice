80c954ca03380b309556c3fffb7ef9abc2906575
Vikas Sharma <sharmavikas3525@gmail.com>
2026-08-29 18:51:16 +0530

    Added LCP771

---
+++ LCP771.java
@@ -0,0 +1,19 @@
+//LCP771: Jewels and Stones
+//https://leetcode.com/problems/jewels-and-stones/
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        boolean[] isJewels = new boolean[128];
+
+        for (char ch : jewels.toCharArray()) {
+            isJewels[ch] = true;
+        }
+
+        int count = 0;
+        for (char ch : stones.toCharArray()) {
+            if (isJewels[ch]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
