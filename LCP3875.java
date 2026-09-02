280100804b207a0821da0fc90532785e203e778f
Vikas Sharma <sharmavikas3525@gmail.com>
2026-09-02 20:06:52 +0530

    Added LCP3875

---
+++ LCP3875.java
@@ -0,0 +1,6 @@
+//LCP3875: https://leetcode.com/problems/construct-uniform-parity-array-i/?envType=daily-question&envId=2026-09-02
+class Solution {
+    public boolean uniformArray(int[] nums1) {
+        return true;
+    }
+}
