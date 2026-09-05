9a272cd0f6206c5de0eb6fe68fa9fc59eacf09f2
Vikas Sharma <sharmavikas3525@gmail.com>
2026-09-05 18:45:30 +0530

    Added LCP3904

---
+++ LCP3904.java
@@ -0,0 +1,21 @@
+//LCP3904: https://leetcode.com/problems/smallest-stable-index-ii/?envType=daily-question&envId=2026-09-05
+class Solution {
+    public int firstStableIndex(int[] nums, int k) {
+
+       int n = nums.length;
+        int[] minValue = new int[n];
+        minValue[n - 1] = nums[n - 1];
+        for (int i = n - 2; i >= 0; i--) {
+            minValue[i] = Math.min(minValue[i + 1], nums[i]);
+        }
+
+        int maxValue = 0;
+        for (int i = 0; i < n; i++) {
+            maxValue = Math.max(maxValue, nums[i]);
+            if (maxValue - minValue[i] <= k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
