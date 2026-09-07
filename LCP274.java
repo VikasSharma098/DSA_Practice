8f3ac1df86f5f13d9733bc91c8d1f6bf5486d021
Vikas Sharma <sharmavikas3525@gmail.com>
2026-09-07 22:30:45 +0530

    Added LCP274

---
+++ LCP274.java
@@ -0,0 +1,23 @@
+//LCP274: H-Index
+//https://leetcode.com/problems/h-index/
+class Solution {
+    public int hIndex(int[] citations) {
+        int len = citations.length;
+        int[] count = new int[len + 1];
+
+        for (int c : citations)
+            if (c > len)
+                count[len]++;
+            else
+                count[c]++;
+
+        int total = 0;
+        for (int i = len; i >= 0; i--) {
+            total += count[i];
+            if (total >= i)
+                return i;
+        }
+
+        return 0;
+    }
+}
